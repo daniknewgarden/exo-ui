53e0ed92cae8be66833093b41f83f041d686e4b5
Danik <daniknewgarden@gmail.com>
2021-03-29 17:25:40 +0300

    test(Button): add new tests

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,42 @@
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    console.log('test')
+import React from 'react'
+
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react'
+
+import { Button } from './index'
+
+describe('Button component tests', () => {
+  it('Render text', () => {
+    const text = 'Button'
+
+    const { getByText } = render(
+      <Button text={text} onClick={() => console.log('click')} />
+    )
+    const buttonElement = getByText(text)
+    expect(buttonElement).toBeInTheDocument()
+  })
+  it('Aria label render', () => {
+    const ariaLabel = 'Some aria label'
+
+    const { getByLabelText } = render(
+      <Button
+        ariaLabel={ariaLabel}
+        text='text'
+        onClick={() => console.log('click')}
+      />
+    )
+    const buttonElement = getByLabelText(ariaLabel)
+    expect(buttonElement).toBeInTheDocument()
+  })
+  it('Click event', () => {
+    const handleClick = jest.fn()
+    const text = 'text'
+
+    const { getByText } = render(<Button text={text} onClick={handleClick} />)
+    const buttonElement = getByText(text)
+
+    userEvent.click(buttonElement)
+    expect(handleClick).toHaveBeenCalledTimes(1)
   })
 })
