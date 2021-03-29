5f13c4bc05f358a1a74d2505d0da66859525124f
Danik <daniknewgarden@gmail.com>
2021-03-29 17:24:33 +0300

    style(components): add GlobalStyle component
    css variables will be stored there

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-text: #fff;
+    --color-main: #3366ff;
+    --color-darker: #254eda;
+    --color-lighter: #6690ff;
+    --color-disabled: #e3e4eb;
+  }
+`
