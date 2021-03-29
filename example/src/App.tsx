1b600ceefd4f7c230a4d77f7ec4718bb1496f326
Danik <daniknewgarden@gmail.com>
2021-03-29 17:26:40 +0300

    feat(example): add Button examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,64 @@
 import React from 'react'
 
-import { ExampleComponentStyled } from 'exo-design-system'
+import { Button } from 'exo-design-system'
+
+import PlusIcon from './assets/Icon.svg'
+import ArrowIcon from './assets/Arrow.svg'
 
 const App = () => {
-  return <ExampleComponentStyled text='Create React Library Example 😄' />
+  return (
+    <>
+      <div className='App'>
+        <Button text='Default' onClick={() => console.log('click')} />
+        <Button text='Rounded' onClick={() => console.log('click')} rounded />
+        <Button
+          text='Transparent'
+          onClick={() => console.log('click')}
+          transparent
+        />
+        <Button
+          text='Text & icon'
+          icon={ArrowIcon}
+          onClick={() => console.log('click')}
+        />
+        <Button
+          text='Text & icon'
+          icon={ArrowIcon}
+          onClick={() => console.log('click')}
+          reversed
+        />
+        <Button
+          icon={PlusIcon}
+          onClick={() => console.log('click')}
+          ariaLabel='add something'
+        />
+        <Button
+          icon={PlusIcon}
+          onClick={() => console.log('click')}
+          ariaLabel='add something'
+          big
+        />
+        <Button text='Big' onClick={() => console.log('click')} big />
+        <Button
+          text='Text & icon'
+          icon={ArrowIcon}
+          onClick={() => console.log('click')}
+          disabled
+        />
+        <Button
+          text='Text & icon'
+          icon={ArrowIcon}
+          onClick={() => console.log('click')}
+          reversed
+        />
+        <Button
+          icon={PlusIcon}
+          onClick={() => console.log('click')}
+          ariaLabel='add something'
+        />
+      </div>
+    </>
+  )
 }
 
 export default App
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -3,5 +3,12 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { GlobalStyle } from 'exo-design-system'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+)
