5b9c3a1d7b2333b1fbf478000fbdb8e197e9a675
Danik <daniknewgarden@gmail.com>
2021-05-04 22:38:26 +0300

    style(storybook): add dark theme (by system preferences)

--- example/.storybook/preview.js
+++ example/.storybook/preview.js
@@ -1,10 +1,6 @@
+// Global styles
+import '../src/stories/global.css'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  actions: { argTypesRegex: '^on[A-Z].*' }
 }
