af23428a582f65cbc97f943b631630df0885b421
daniknewgarden <work.daniknewgarden@gmail.com>
2021-05-06 19:23:56 +0300

    fix(components): add ButtonProps import

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,6 @@ export { GlobalStyle } from './components/GlobalStyle'
 
 // Components
 export { ButtonStyled as Button } from './components/Button'
+
+// Prop types
+export type { ButtonProps } from './components/Button'
